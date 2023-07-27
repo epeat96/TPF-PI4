1c61fedcc0036ba320c62bbbe0800e1bcb37a79b
Progra 4 <progra4@test.com>
2023-07-27 08:44:19 -0400

    Quita ñ de procedimiento almacenado para evitar problemas xd

--- SQL/Procedimientos Almacenados/ObtenerLegajosEmpleadosNoLiquidadosEsteMes.sql
+++ SQL/Procedimientos Almacenados/ObtenerLegajosEmpleadosNoLiquidadosEsteMes.sql
@@ -5,16 +5,16 @@ END IF;
 CREATE PROCEDURE ObtenerLegajosEmpleadosNoLiquidadosEsteMes()
 BEGIN
     DECLARE @MesActual INT;
-    DECLARE @AñoActual INT;
+    DECLARE @AnoActual INT;
 
     SET @MesActual = MONTH(GETDATE());
-    SET @AñoActual = YEAR(GETDATE());
+    SET @AnoActual = YEAR(GETDATE());
 
     SELECT e.legajo 
     FROM empleados e
     LEFT JOIN liquidaciones l ON e.legajo = l.legajo
         AND MONTH(l.fecha_inicial) = @MesActual
-        AND YEAR(l.fecha_inicial) = @AñoActual
+        AND YEAR(l.fecha_inicial) = @AnoActual
     WHERE l.legajo IS NULL
     AND e.fecha_salida IS NULL
     AND e.salario_actual IS NOT NULL;
