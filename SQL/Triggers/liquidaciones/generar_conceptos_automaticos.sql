59bec42ed205a08726e6531e0b85bbc4d6a681c0
Progra 4 <progra4@test.com>
2023-07-27 19:16:15 -0400

    Agrega ; donde falta

--- SQL/Triggers/liquidaciones/generar_conceptos_automaticos.sql
+++ SQL/Triggers/liquidaciones/generar_conceptos_automaticos.sql
@@ -30,13 +30,13 @@ BEGIN
         monto_ips_obrero = DBA.CalcularIPSObrero(@monto_total),
         bonificacion_familiar = DBA.CalcularBonificacionFamiliar(@legajo),
         total_aguinaldo = @monto_total/12  
-    WHERE liquidacion = nueva_liquidacion.liquidacion 
+    WHERE liquidacion = nueva_liquidacion.liquidacion;
 
     -- Insertar la liquidacion automatica del Salario 
     INSERT INTO "DBA"."LIQUIDACIONES_DETALLES" ("LIQUIDACION","CONCEPTO","MONTO","SIGNO","FECHA")
-    VALUES(nueva_liquidacion.liquidacion,1,@salario_actual,'S',GetDate())
+    VALUES(nueva_liquidacion.liquidacion,1,@salario_actual,'S',GetDate());
     -- Insertar la liquidacion automatica de la bonificacion familiar
     INSERT INTO "DBA"."LIQUIDACIONES_DETALLES" ("LIQUIDACION","CONCEPTO","MONTO","SIGNO","FECHA")
-    VALUES(nueva_liquidacion.liquidacion,2,DBA.CalcularBonificacionFamiliar(@legajo),'S',GetDate())
+    VALUES(nueva_liquidacion.liquidacion,2,DBA.CalcularBonificacionFamiliar(@legajo),'S',GetDate());
 
 END;
