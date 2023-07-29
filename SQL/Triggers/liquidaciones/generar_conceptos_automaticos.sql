6c92938fe24d9a0d31a3b7c46b37a46f58eeb738
Epeat <44922559+epeat96@users.noreply.github.com>
2023-07-29 18:41:28 -0400

    modifica script de trigger de conceptos automaticos

--- SQL/Triggers/liquidaciones/generar_conceptos_automaticos.sql
+++ SQL/Triggers/liquidaciones/generar_conceptos_automaticos.sql
@@ -13,6 +13,12 @@ BEGIN
     DECLARE @liquidacion INTEGER ;
     DECLARE @salario_actual NUMERIC(12,2);
     DECLARE @monto_total NUMERIC(12,2);
+    DECLARE @monto_para_ips NUMERIC(12,2);
+    DECLARE @monto_para_aguinaldo NUMERIC(12,2);
+    DECLARE @salario_aplica_ips CHARACTER;
+    DECLARE @salario_aplica_aguinaldo CHARACTER
+    DECLARE @bonificacion_familiar_aplica_ips CHARACTER;
+    DECLARE @bonificacion_familiar_aplica_aguinaldo CHARACTER
 
     SET @legajo = nueva_liquidacion.legajo;
     SET @liquidacion = nueva_liquidacion.liquidacion;
@@ -22,12 +28,31 @@ BEGIN
     FROM EMPLEADOS
     WHERE legajo = @legajo;
 
+
+    SELECT aplica_ips,aplica_aguinaldo INTO @salario_aplica_ips,@salario_aplica_aguinaldo
+    FROM CONCEPTOS
+    WHERE concepto = 1
+
+    SELECT aplica_ips,aplica_aguinaldo INTO @bonificacion_familiar_aplica_ips,@bonificacion_familiar_aplica_aguinaldo
+    FROM CONCEPTOS
+    WHERE concepto = 2
+
+    @monto_para_ips = 0
+    
+    IF @salario_aplica_ips = 's' THEN
+        @monto_para_ips = @monto_para_ips + @salario_actual
+    END IF
+
+    IF @bonificacion_familiar_aplica_ips = 's' THEN
+        @monto_para_ips = @monto_para_ips + DB.CalcularBonificacionFamiliar(@legajo)
+    END IF
+
     SET @monto_total = (@salario_actual + DBA.CalcularBonificacionFamiliar(@legajo));
 
     UPDATE liquidaciones
     SET monto_total = @monto_total,
-        monto_ips_patronal = DBA.CalcularIPSPatronal(@monto_total),
-        monto_ips_obrero = DBA.CalcularIPSObrero(@monto_total),
+        monto_ips_patronal = DBA.CalcularIPSPatronal(@monto_para_ips),
+        monto_ips_obrero = DBA.CalcularIPSObrero(@monto_para_ips),
         bonificacion_familiar = DBA.CalcularBonificacionFamiliar(@legajo),
         total_aguinaldo = @monto_total/12  
     WHERE liquidacion = nueva_liquidacion.liquidacion;
